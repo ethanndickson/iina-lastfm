dbc4adc0998d4953e669f66e20d7a5d568f68b56
Ethan Dickson <ethanndickson@gmail.com>
2021-08-25 02:51:59 +1000

    Initial commit

---
+++ lastfmscrobbler/scrobble.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+use JSON;
+use Data::Dumper;
+use LWP::UserAgent;
+use Digest::MD5 qw(md5_hex);
+use Encode qw(encode);
+
+sleep(3);
+$ua = LWP::UserAgent->new();
+$lfmUrl = "http://ws.audioscrobbler.com/2.0";
+
+$USERNAME = ""; # CHANGE ME
+$PASSWORD = ""; # CHANGE ME
+$APIKEY = "3176eb0bd0ff5d1c8f15d94e3b3c98a8"; # Change this if it stops working
+$APISECRET = "2918fac730f44f543d2568f9976ec276"; # Change this if it stops working
+
+
+# read from socket
+$metaDataRaw = `echo '{ "command": ["get_property", "metadata"] }' | nc -U  /tmp/mpv-socket`; 
+$durationRaw = `echo '{ "command": ["get_property", "duration"] }' | nc -U  /tmp/mpv-socket`;
+$metaData = decode_json $metaDataRaw;
+$durationData = decode_json $durationRaw; 
+$mediaTitle = $metaData->{'data'}->{'Title'};
+if (not $mediaTitle) {
+	$mediaTitle = $metaData->{'data'}->{'title'};
+}
+$mediaArtist = $metaData->{'data'}->{'Artist'};
+if (not $mediaArtist) {
+	$mediaArtist = $metaData->{'data'}->{'artist'};
+}
+$mediaAlbum = $metaData->{'data'}->{'Album'};
+if (not $metaData) {
+	$metaData = $metaData->{'data'}->{'album'};
+}
+$mediaDuration = $durationData->{'data'};
+$mediaTimestamp = time();
+if (not $mediaTitle) {
+	die;
+}
+if (not $mediaArtist) {
+	die;
+}
+if (not $mediaDuration) {
+	die;
+}
+$authSig = md5_hex("api_key".$APIKEY."method"."auth.getMobileSession"."password".$PASSWORD."username".$USERNAME.$APISECRET);
+$auth = $lfmUrl."?method=auth.getMobileSession&username=".$USERNAME."&password=".$PASSWORD."&api_key=".$APIKEY."&api_sig=".$authSig;
+$authOutput = $ua->post($auth);
+$authOutput = Dumper($authOutput);
+if ($authOutput =~ /<key>(.*?)<\/key>/) {
+	$sessionKey = $1;
+} else {
+	die();
+}
+if ($mediaAlbum) { # if the track metadata has an album tied to it
+	$scrobbleSig = md5_hex("album".$mediaAlbum."api_key".$APIKEY."artist".$mediaArtist."method"."track.scrobble"."sk".$sessionKey."timestamp".$mediaTimestamp."track".$mediaTitle.$APISECRET);
+	$scrobbleUrl = $lfmUrl."?method=track.scrobble&artist=".$mediaArtist."&track=".$mediaTitle."&timestamp=".$mediaTimestamp."&album=".$mediaAlbum."&api_key=".$APIKEY."&api_sig=".$scrobbleSig."&sk=".$sessionKey;
+} else { 
+	$scrobbleSig = md5_hex("api_key".$APIKEY."artist".$mediaArtist."method"."track.scrobble"."sk".$sessionKey."timestamp".$mediaTimestamp."track".$mediaTitle.$APISECRET);
+	$scrobbleUrl = $lfmUrl."?method=track.scrobble&artist=".$mediaArtist."&track=".$mediaTitle."&timestamp=".$mediaTimestamp."&api_key=".$APIKEY."&api_sig=".$scrobbleSig."&sk=".$sessionKey;
+}
+$scrobbleOutput = $ua->post($scrobbleUrl);
+exit();
---
+++ scripts/on-change.lua
@@ -0,0 +1,6 @@
+function track_load()
+    -- local data = mp.get_property_native("metadata") # This is nil for some reason, but not in mpv...
+    os.execute("perl ~/.config/mpv/lastfmscrobbler/scrobble.pl")
+end
+
+mp.register_event("start-file", track_load)
